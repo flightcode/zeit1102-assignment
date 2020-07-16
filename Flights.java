2863aaaab8d17c31fda2469147c30b9e8c0118c1
Elliot Parker <elliotccp@protonmail.com>
2020-07-16 10:25:45 +1000

    Add Flights init menu

--- Flights.java
+++ Flights.java
@@ -6,5 +6,14 @@ public class Flights
 {
     public void Init()
     {
+        System.out.println("Select Option:");
+        System.out.println("1. View Flights");
+        System.out.println("2. Add Flight");
+        System.out.println("3. Delete Flight");
+    }
+    
+    void View()
+    {
+        System.out.println("Viewing Flights");
     }
 }
