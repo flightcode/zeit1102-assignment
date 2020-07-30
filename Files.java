34bc653a2ca1152f05bdb7c42c38b5178cc975f0
Elliot Parker <elliotccp@protonmail.com>
2020-07-30 16:05:33 +1000

    Add write function

--- Files.java
+++ Files.java
@@ -12,10 +12,20 @@ public class Files {
         }
         Scanner scan = new Scanner(input); // Scan file
         while (scan.hasNextLine()) { // Scan each line of file
-            Flight newFlt = new Flight(scan.nextLine().split(",")); // Split line by "," and generate flight
+            Flight newFlt = new Flight(scan.nextLine().split(", ")); // Split line by "," and generate flight
             arr.add(newFlt); // Add flight to array
         }
         scan.close();
         return arr; // Return Array List
+    }
+
+    public void Write(ArrayList<Flight> flights, String outputFile) throws Exception {
+        BufferedWriter output = new BufferedWriter(new FileWriter(outputFile)); // Declare save file
+        output.createNewFile(); // Try creating file to write to
+        for (int i = 0; i < flights.size(); i++) { // Iterate through all flights in array
+            file.write(Arrays.toString(flights.get(i))); // Write flight to file, delimited by ", "
+            file.newLine(); // Go to next line
+        }
+        file.close();
     }
 }
