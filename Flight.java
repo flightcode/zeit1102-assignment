9436edba81019969e94dd92e8a06a4d8c7be111b
Elliot Parker <elliotccp@protonmail.com>
2020-07-30 15:54:12 +1000

    Add object creation methods

--- Flight.java
+++ Flight.java
@@ -13,4 +13,22 @@ public class Flight {
 
     public Flight() {
     }
+
+    public Flight(String dep, String arr, String airline, String fltNum, String acftType, String acftReg) {
+        this.dep = dep;
+        this.arr = arr;
+        this.airline = airline;
+        this.fltNum = fltNum;
+        this.acftType = acftType;
+        this.acftReg = acftReg;
+    }
+
+    public Flight(String[] arr) {
+        this.dep = arr[0];
+        this.arr = arr[1];
+        this.airline = arr[2];
+        this.fltNum = arr[3];
+        this.acftType = arr[4];
+        this.acftReg = arr[5];
+    }
 }
